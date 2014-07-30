92a4f30704e5e548cdca0e3291cc8e609db70060
Noam Lewis <noaml@bdbpayroll.com>
2014-07-30 14:55:29 -0400

    add missing file

---
+++ WhitelistExecuter.Web/App_Start/ErrorHandler.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using log4net;
+
+namespace WhitelistExecuter.Web.App_Start
+{
+    public class ErrorHandler : HandleErrorAttribute
+    {
+        protected static readonly ILog _logger = log4net.LogManager.GetLogger("ErrorHandler");
+
+        public override void OnException(ExceptionContext filterContext)
+        {
+            _logger.Error("ErrorHandler handled error", filterContext.Exception);
+            base.OnException(filterContext);
+        }
+    }
+}
